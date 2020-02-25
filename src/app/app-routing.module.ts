f323266c8459fa2ec74d5640ca43ffb328426a97
y4m4t0 <argun63901@gmail.com>
2020-02-25 11:47:10 +0600

    updated version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryListComponent } from './category-list/category-list.component';
-import { ProductItemComponent } from './product-item/product-item.component';
-import { SamsungItemComponent } from './samsung-item/samsung-item.component';
-import { IphoneItemComponent } from './iphone-item/iphone-item.component';
-import { XiaomiItemComponent } from './xiaomi-item/xiaomi-item.component';
-import { HuaweiItemComponent } from './huawei-item/huawei-item.component';
 import { ProductItemDetailsComponent } from './product-item-details/product-item-details.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductItemComponent } from './product-item/product-item.component';
-import { ProductItemSliderComponent } from './product-item-slider/product-item-slider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CategoryListComponent } from './category-list/category-list.component';
-import { SamsungItemComponent } from './samsung-item/samsung-item.component';
-import { IphoneItemComponent } from './iphone-item/iphone-item.component';
-import { XiaomiItemComponent } from './xiaomi-item/xiaomi-item.component';
-import { HuaweiItemComponent } from './huawei-item/huawei-item.component';
 import { ProductService } from './product.service';
 import { CategoriesService } from './categories.service';
 import { ProductItemDetailsComponent } from './product-item-details/product-item-details.component';
@@ -21,12 +16,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   declarations: [
     AppComponent,
     ProductItemComponent,
-    ProductItemSliderComponent,
     CategoryListComponent,
-    SamsungItemComponent,
-    IphoneItemComponent,
-    XiaomiItemComponent,
-    HuaweiItemComponent,
     ProductItemDetailsComponent,
     DashboardComponent,
   ],
